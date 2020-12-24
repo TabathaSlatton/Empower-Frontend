78ca26df4181509cee94a1d51f9f4fb8cf9dc992
Tabatha Slatton <valbatha@gmail.com>
2020-12-23 20:06:33 -0700

    building logic for login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login';
 // import Signup from './components/Signup';
 
 class App extends Component {
+  
   state = {
     user: {
       id: null, 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,50 @@
 import React, { Component} from 'react';
 
 export default class Login extends Component {
+   
+    // state makes this a controlled component
+    state = {
+        email: "",
+        password: ""
+    }
 
+    handleChange = (e) => this.setState({[e.target.name]: e.target.value})
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const { email, password} = this.state
+        // backend is expecting email and password params
+        const body = {email: email, password: password}
+        fetch('http://localhost:3000/api/v1/logiv', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+        })
+        .then(resp => resp.json())
+        .then()
+    }
+
+    // fetch('http://localhost:3000/api/v1/profile', {
+    //     method: 'GET',
+    //     headers: {
+    //     Authorization: `Bearer <token>`
+    //      }
+    // })
 
     render(){
         return(
             <>
                 <h1>This is my Login Component</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>
                         Email:
-                        <input type="email" name="email" />
+                        <input type="email" name="email" onChange={this.handleChange} value={this.state.email} />
                     </label>
                     <label>
                         Password:
-                        <input type="password" name="password" />
+                        <input type="password" name="password" onChange={this.handleChange} value={this.state.password}/>
                     </label>
                     <input type="submit" value="Login" />
                 </form>
