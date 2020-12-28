34f0ad825ba079869e1d98c3b606b60f93523c67
Tabatha Slatton <valbatha@gmail.com>
2020-12-27 18:02:01 -0700

    started using redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
