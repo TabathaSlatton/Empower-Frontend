48fab6771a6675fa2b5625279eff0e1b6cd7d4fd
Tabatha Slatton <valbatha@gmail.com>
2021-01-12 15:25:13 -0500

    added styling to homepage and signup/login

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,18 +18,18 @@ const Footer = () => {
                     <Col>
                     <h3>Featured</h3>
                         <ListGroup className="footer-list">
-                            <ListGroup.Item className="bg-transparent border-0">253-320-6936</ListGroup.Item>
-                            <ListGroup.Item className="bg-transparent border-0">Lawrence, NJ</ListGroup.Item>
-                            <ListGroup.Item className="bg-transparent border-0">valbatha@gmail.com</ListGroup.Item>
+                            <ListGroup.Item action className="bg-transparent border-0" href="https://www.forbes.com/power-women/#13249bad5e25" target="_blank" style={{color: "darkgray"}}>Most Influential Women of 2020</ListGroup.Item>
+                            <ListGroup.Item action className="bg-transparent border-0" href="https://clintonwhitehouse4.archives.gov/women/gresources.html" target="_blank" style={{color: "darkgray"}}>Government Resources for Women</ListGroup.Item>
+                            {/* <ListGroup.Item action className="bg-transparent border-0" href="https://www.forbes.com/power-women/#13249bad5e25" target="_blank" style={{color: "darkgray"}}>valbatha@gmail.com</ListGroup.Item> */}
                         </ListGroup>
                     </Col>
 
                     <Col>
                     <h3>More From Me</h3>
                         <ListGroup className="footer-list">
-                            <ListGroup.Item className="bg-transparent border-0">253-320-6936</ListGroup.Item>
-                            <ListGroup.Item className="bg-transparent border-0"> Lawrence, NJ</ListGroup.Item>
-                            <ListGroup.Item className="bg-transparent border-0">valbatha@gmail.com</ListGroup.Item>
+                        <ListGroup.Item action className="bg-transparent border-0" href="https://www.forbes.com/power-women/#13249bad5e25" target="_blank" style={{color: "darkgray"}}>GitHub</ListGroup.Item>
+                            <ListGroup.Item action className="bg-transparent border-0" href="https://clintonwhitehouse4.archives.gov/women/gresources.html" target="_blank" style={{color: "darkgray"}}>Blog</ListGroup.Item>
+                            <ListGroup.Item action className="bg-transparent border-0" href="https://www.forbes.com/power-women/#13249bad5e25" target="_blank" style={{color: "darkgray"}}>LinkedIn</ListGroup.Item>
                         </ListGroup>
                     </Col>
                 </Row>
