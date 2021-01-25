6798b0e8ae8e070d99e9224b2c74c07b8fefe005
Tabatha Slatton <valbatha@gmail.com>
2021-01-25 16:14:16 -0500

    improved profile page styling

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -1,6 +1,9 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux'
 import ProfileForm from '../components/ProfileForm';
+import {Image, Col, Row, Container } from 'react-bootstrap'
+
+
 
 
 class ProfilePage extends Component {
@@ -8,19 +11,35 @@ class ProfilePage extends Component {
     render(){
         return(
             <div>
-                <section id="profile-heading" className="p-5">
+                <section id="profile-form-background" className="p-5">
                     <div className="dark-overlay">
                         <h1 id="heading-title">Edit Profile</h1>       
                 <ProfileForm/>
                 </div>
                 </section><br/><br/>
-
-                <section id="home-heading" className="p-5">
-                    <div className="dark-overlay">
-                        <h1 id="heading-title">{this.props.user.first_name} {this.props.user.last_name}'s Profile</h1>     
-                    </div>
+                <section id="profile-heading" className="p-5">
+                    {/* <div className="dark-overlay"> */}
+                        {/* <img src={this.props.user.profile_img_url}/> */}
+                        <Container>
+                            <Row>
+                                <Col xs={6} md={2}>
+                                    {/* <p> test</p> */}
+                                    <Image src={this.props.user.profile_img_url} thumbnail roundedCircle />
+                                </Col>
+                                <Col xs={6} md={10}>
+                                    <h1 id="profile-heading-title">{this.props.user.first_name} {this.props.user.last_name}'s Profile </h1>
+                                </Col>
+                                
+                                {/* <Col xs={6} md={4}>
+                                <Image src="holder.js/171x180" roundedCircle />
+                                </Col>
+                                <Col xs={6} md={4}>
+                                <Image src="holder.js/171x180" thumbnail />
+                                </Col> */}
+                            </Row>
+                        </Container>
+                    {/* </div> */}
                 </section><br/><br/>
-                <img src={this.props.user.profile_img_url}/>
                 { this.props.user.point_wallet ?
                     <h3>You have {this.props.user.point_wallet} points available.</h3>
                 :
